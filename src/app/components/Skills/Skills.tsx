5f19fb945bd7fc863d70e4ea767b9cffd4f507b3
themchoise <140093681+ttinydeveloper@users.noreply.github.com>
2023-11-27 18:25:00 -0300

    up

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -1,16 +1,20 @@
+'use client'
 import React from 'react'
 import FrontEnd from './FrontEnd'
+import { returnAllSkills } from '@/app/mock/jobHistory'
 
 const Skills = () => {
+console.log('here')
+  console.log ( returnAllSkills() )
+
   return (
     <div>
-        <article className="bg-dark-graf-nav shadow-[0px_0px_0px_0.2px_#f7fafc]">
-    <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2">
-    <div className="text-cyan-50 text-justify m-2">
-        <FrontEnd/>
-    </div>
-    <div className="m-2">
-   
+        <article className="bg-dark-graf-nav shadow-[0px_0px_0px_0.2px_#f7fafc] w-auto h-[500px] ">
+          <div className='flex p-5'>
+          <div className='px-2 pt-4 pb-2 flex  overflow-auto w-auto h-[500px] '>
+    {returnAllSkills().map( (res,index) => (
+      <span key={index} className=" m-3 bg-gray-200 rounded-full h-10 px-3 py-1 text-sm font-semibold text-gray-700 ">#{res}</span>
+    ))}
     </div>
     </div>
    
--- src/app/mock/jobHistory.ts
+++ src/app/mock/jobHistory.ts
@@ -15,6 +15,19 @@ const JBOSS :string = 'JBOSS'
 const WILDFLY :string = 'Wildfly'
 const ELECTRONJS :string = 'ElectronJS'
 const MYSQL: string = 'MYSQL'
+const MSSQL: string = 'MSSQL'
+const HTML: string = 'HTML'
+const CSS: string = 'CSS'
+const REDUX: string = 'Redux'
+const JQUERY: string = 'JQuery'
+const MUI: string = 'MUI'
+const BOOSTRAP: string = 'Boostrap'
+const MONGODB: string = 'Mongo DB'
+const NGINX: string = 'NGINX'
+const MIKROTIK: string = 'Mikrotik'
+const FORTIGATE: string = 'Fortigate'
+const WINDOWSSERVER: string = 'Windows Server'
+const UBUNTUSERVER: string = 'Ubuntu Server'
 
 export interface IJobHistory{
     year_start: number
@@ -49,15 +62,40 @@ const jobHistory:IJobHistory[] = [
     },
     {
         year_start: 2021,
-        year_end: 2021,
+        year_end: 2022,
         company: 'Sportclub',
         job_details: 'Web Developer Full Stack',
-        job_techs: ['Java', 'SpringBoot', 'Spring Framework', 'Spring Security', 'JBOSS', 'Wildfly', 'NodeJS', ELECTRONJS]
+        job_techs: [REACT, ELECTRONJS, MYSQL,MSSQL, JAVASCRIPT, REDUX, HTML, CSS, JQUERY, MUI, BOOSTRAP, ELECTRONJS, MONGODB  ]
+    },
+    {
+        year_start: 2013,
+        year_end: 2021,
+        company: 'Sportclub',
+        job_details: 'SysAdmin, Networking, Infraestructura',
+        job_techs: [NGINX,MIKROTIK, FORTIGATE, WINDOWSSERVER, UBUNTUSERVER  ]
     }
 
 
 
 ]
 
+export const returnAllSkills = ():Array<string> => {
+
+    let newArr = []
+    const techs = jobHistory.map(job => job.job_techs)
+
+    for (let i = 0; i < techs.length; i++) {
+        const elm = techs[i];
+        for (let j = 0; j < elm.length; j++) {
+            const elminter = elm[j];
+            newArr.push(elminter)
+        }
+    }
+    console.log(newArr)
+    return [...new Set(newArr)]
+
+}
+
 
 export default jobHistory
+
