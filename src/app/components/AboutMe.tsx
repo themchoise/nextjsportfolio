24759ea5cc7962ee14ce4724fe4f330ada0e3126
ttinydeveloper <maximilianojoyce@sportclub.team>
2023-11-29 08:11:13 -0300

    test

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,8 +1,36 @@
+"use client"
 import aboutme from "../mock/information";
 import Image from 'next/image'
-
+import React, { useEffect, useState } from 'react';
 
  const AboutMe = () => {
+
+  const [effect, setEffect]  = useState('')
+  const [position, setPosition]  = useState(0)
+
+  const colorList:string[] = ['#f7fafc', '#3A6166  ','#454B1B']
+  const rndNumber = () => {
+    const intervalNumbers:number = colorList.length 
+    if (position === intervalNumbers) {
+      setPosition(0);
+      return 0;
+    };
+    setPosition(position + 1)
+    return position
+   }
+
+
+  console.log(effect)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(rndNumber())
+      setEffect(colorList[rndNumber() ]) 
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [colorList]);
+  
+
   return (
     <>
     
@@ -11,7 +39,9 @@ import Image from 'next/image'
     <div className="text-cyan-50 text-justify m-2 shadow-[0px_0px_0px_0.2px_#f7fafc]  bg-dark-graf-nav p-5">
         {aboutme}
     </div>
-    <div className="m-2 shadow-[0px_0px_0px_0.2px_#f7fafc]  bg-dark-graf-nav flex justify-center p-5 	object-contain	">
+    <div className={`hover:shadow-[0px_0px_0px_2px_${effect}] transition duration-1000 ease-in-out animate-colorChange m-2 shadow-[0px_0px_0px_0.2px_#f7fafc]  bg-dark-graf-nav flex justify-center p-5 	object-contain	`}
+   
+    >
     <Image 
       className ='object-contain'
       src="/random1.jpg"
