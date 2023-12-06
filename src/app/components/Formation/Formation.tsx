4cef3769c3a4fdf357eb3152761b6a3815ba264f
ttinydeveloper <maximilianojoyce@gmail.com>
2023-12-06 07:28:50 -0300

    up

--- src/app/components/Formation/Formation.tsx
+++ src/app/components/Formation/Formation.tsx
@@ -1,28 +1,88 @@
 "use client"
+import { useEffect, useState } from 'react';
 import formationArr, { IFormation } from '../../mock/formation';
-import { FormationCard } from './FormationCard';
+
 
 
 
 export const Formation = () => {
-  return (<>
-  {formationArr.map((formation:IFormation , index) => 
 
-  (
-  <div key={index}>
-  <FormationCard key={index}
-    academy = {formation.academy}
-    titulation = {formation.titulation}
-    grade = {formation.grade}
-    year_start = {formation.year_start}
-    year_end = {formation.year_end}
-    cred_id = {formation.cred_id}
-    cred_url = {formation.cred_url}
-    />
-    </div>
-    
-    )
+
+  const handleSort = (data:IFormation[], sortitem:any) => data.sort((a:any,b:any) =>  b[sortitem] - a[sortitem] )
+
+
+  interface IFormationState{
+    sortedby:string
+    data:IFormation[] 
+  }
+
+  const initialState:IFormationState = {
+    sortedby:'',
+    data: formationArr //handleSort(formationArr, 'year_end')
+  }
+
+
+  const [sortedBy, setSortedBy] = useState<IFormationState>(initialState)
+
+
+
+
+const handleSortClick = (sortItem) =>{
+  setSortedBy(prev => {return {...prev, sortedby:'grade', data:handle
+  Sort(sortedBy.data, 'grade')}})
+
+}
+
+console.log(sortedBy)
+
+
+  return (<>
+<div className='flex  justify-center items-center w-full '>
+<table className="md:table-fixed    text-white w-auto">
+  <thead>
+    <tr className='text-cyan-600'>
+      <th onClick={ (e) =>  handleSortClick(e) }>Inicio</th>
+      <th>Fin</th>
+      <th>Academia</th>
+      <th>Titulacion</th>
+      <th>Grado</th>
+      <th>Cred Id</th>
+      <th>Cred URL</th>
+    </tr>
+  </thead>
+  <tbody>
+  {sortedBy.data.map((formation:IFormation , index) => 
+
+(
+     <tr className='border-b justify-center text-center' key={index}>
+        <td className='p-3'>
+          {formation.year_start}
+        </td>
+        <td className='p-3'>
+          {formation.year_end}
+        </td>
+        <td className='p-3'>
+          {formation.academy}
+        </td>
+        <td className='w-auto p-3'>
+          {formation.titulation}
+        </td>
+        <td className='w-auto p-3'>
+          {formation.grade}
+        </td>
+        <td className='p-3'>
+          {formation.cred_id}
+        </td>
+        <td className='p-3'>
+        {formation.cred_url.length>0 && <a href= {formation.cred_url}> Link </a>  }
+        </td>
+     </tr>
+  )
+
+)}
+  </tbody>
+</table>
+</div>
   
-  )}
   </>           );
 };
--- src/app/mock/formation.ts
+++ src/app/mock/formation.ts
@@ -74,6 +74,96 @@ const formationArr: IFormation[] = [
         cred_id: '',
         cred_url: ''
     }
+    ,
+    {
+        academy: 'Linkedin',
+        titulation: 'Javascript',
+        grade: 'Course',
+        year_start: 2020,
+        year_end: 2020,
+        cred_id: '',
+        cred_url: 'https://www.linkedin.com/learning/certificates/a8cad059b7d5a83291277430cd379024baf60331b25723aff676507695163146?trk=backfilled_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVwTJDHkPQKmg5RK6ilC5jQ%3D%3D'
+    }
+    ,
+    {
+        academy: 'Argentina Programa',
+        titulation: 'Java Etapa II',
+        grade: 'Course',
+        year_start: 2022,
+        year_end: 2022,
+        cred_id: '',
+        cred_url: ''
+    }
+    ,
+    {
+        academy: 'Argentina Programa',
+        titulation: 'Java Etapa I',
+        grade: 'Course',
+        year_start: 2021,
+        year_end: 2021,
+        cred_id: '',
+        cred_url: ''
+    }
+    ,
+    {
+        academy: 'Udemy',
+        titulation: 'JavaScript Full',
+        grade: 'Course',
+        year_start: 2021,
+        year_end: 2021,
+        cred_id: 'UC-59c46af9-dd32-4739-965a-ca9c9a1b9e48',
+        cred_url: 'https://www.udemy.com/certificate/UC-59c46af9-dd32-4739-965a-ca9c9a1b9e48/'
+    }
+    ,
+    {
+        academy: 'CoderHouse',
+        titulation: 'JavaScript',
+        grade: 'Course',
+        year_start: 2021,
+        year_end: 2021,
+        cred_id: '617a1fd23088d40045607c27',
+        cred_url: ''
+    }
+    ,
+    {
+        academy: 'Udemy',
+        titulation: 'React',
+        grade: 'Course',
+        year_start: 2022,
+        year_end: 2022,
+        cred_id: 'UC-3312d70f-e13f-45b2-9f11-bd0012b9c68a',
+        cred_url: ''
+    }
+    ,
+    {
+        academy: 'Platzi',
+        titulation: 'Nodejs Api Rest',
+        grade: 'Course',
+        year_start: 2022,
+        year_end: 2022,
+        cred_id: '',
+        cred_url: 'https://platzi.com/p/maximiliano.joyce/curso/2485-backend-nodejs/diploma/detalle/'
+    }
+    ,
+    {
+        academy: 'Platzi',
+        titulation: 'Docker',
+        grade: 'Course',
+        year_start: 2023,
+        year_end: 2023,
+        cred_id: '',
+        cred_url: 'https://platzi.com/p/maximiliano.joyce/curso/2066-docker/diploma/detalle/'
+    }
+    ,
+    {
+        academy: 'Platzi',
+        titulation: 'Nodejs Microservicios Redis',
+        grade: 'Course',
+        year_start: 2023,
+        year_end: 2023,
+        cred_id: '',
+        cred_url: 'https://platzi.com/p/maximiliano.joyce/curso/1767-nodejs-microservicios/diploma/detalle/'
+    }
 ]
 
 
